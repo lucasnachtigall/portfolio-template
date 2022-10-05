9b78a53157b6e4f3fc6f41c39afdce8b06528054
lucasnachtigall <lucas.nachtigall@gmail.com>
2022-10-05 17:46:59 -0300

    Initial commit

---
+++ assets/js/projects.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    text: 'text 0 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus facere aspernatur pariatur quasi totam esse dolores tempore ipsum facilis a?',
+    image: 'https://picsum.photos/id/42/600/400/',
+    url: 'https://picsum.photos/id/42/600/400/',
+  },
+  {
+    text: 'text 1 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus facere aspernatur pariatur quasi totam esse dolores tempore ipsum facilis a?',
+    image: 'https://picsum.photos/id/43/600/400/',
+    url: 'https://picsum.photos/id/43/600/400/',
+  },
+  {
+    text: 'text 2 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus facere aspernatur pariatur quasi totam esse dolores tempore ipsum facilis a?',
+    image: 'https://picsum.photos/id/44/600/400/',
+    url: 'https://picsum.photos/id/44/600/400/',
+  },
+  {
+    text: 'text 3 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus facere aspernatur pariatur quasi totam esse dolores tempore ipsum facilis a?',
+    image: 'https://picsum.photos/id/45/600/400/',
+    url: 'https://picsum.photos/id/45/600/400/',
+  },
+  {
+    text: 'text 4 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus facere aspernatur pariatur quasi totam esse dolores tempore ipsum facilis a?',
+    image: 'https://picsum.photos/id/46/600/400/',
+    url: 'https://picsum.photos/id/46/600/400/',
+  },
+  {
+    text: 'text 5 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus facere aspernatur pariatur quasi totam esse dolores tempore ipsum facilis a?',
+    image: 'https://picsum.photos/id/47/600/400/',
+    url: 'https://picsum.photos/id/47/600/400/',
+  },
+  {
+    text: 'text 6 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus facere aspernatur pariatur quasi totam esse dolores tempore ipsum facilis a?',
+    image: 'https://picsum.photos/id/48/600/400/',
+    url: 'https://picsum.photos/id/48/600/400/',
+  },
+  {
+    text: 'text 7 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus facere aspernatur pariatur quasi totam esse dolores tempore ipsum facilis a?',
+    image: 'https://picsum.photos/id/49/600/400/',
+    url: 'https://picsum.photos/id/49/600/400/',
+  },
+  {
+    text: 'text 8 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus facere aspernatur pariatur quasi totam esse dolores tempore ipsum facilis a?',
+    image: 'https://picsum.photos/id/50/600/400/',
+    url: 'https://picsum.photos/id/50/600/400/',
+  },
+  {
+    text: 'text 9 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus facere aspernatur pariatur quasi totam esse dolores tempore ipsum facilis a?',
+    image: 'https://picsum.photos/id/51/600/400/',
+    url: 'https://picsum.photos/id/51/600/400/',
+  },
+];
